5cdd7cfad5ba21f67949e7a1eecc9fb96f444d27
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-20 10:52:28 -0400

    created my second C script using printf function

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	returnf("with proper grammer, but the outcome is a piece of art,");
-	return 0;
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
 }
