7d645cab7f07f9518512fe64b7ff9269eafc39fd
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-21 11:51:22 -0400

    ran the variable n to see if its negative or positive

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -13,9 +14,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive", n);
-	if else(n == 0)
+	else if(n == 0)
 		printf("%d is zero", n);
 	else
-			printf("%d is negative", n);
+		printf("%d is negative", n);
 	return (0);
 }
