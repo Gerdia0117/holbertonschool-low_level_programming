16b0d45c2085ff47a84c0927ecdd92590db70a3e
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-23 11:42:06 -0400

    changed my comment in the betty dots

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	char letter;
 
-	/* ASCII value of the lowercase letters will loop (a-z)*/
+	/* ASCII value of the lowercase letters with loop (a-z)*/
 	for (letter = 'a'; letter != 'z'; letter++)
-		putchar(letter); /*this prints each letter one by one*/
+		putchar(letter); /*this prints each letter, one by one*/
 
-	putchar('\n'); /*after the alphabet a new line will be printed*/
+	putchar('\n'); /*this prints a newline after the alphaet code*/
 	return (0);
 }
