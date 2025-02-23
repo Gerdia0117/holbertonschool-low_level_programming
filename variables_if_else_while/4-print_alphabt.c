d5c48a0d32bde04191e59646d59fa341a2f8b4ba
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-23 18:48:01 -0400

    removed some comments that were not needed

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <satdio.h>
+/**
+ * main - putchar with condition except for an information
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	putchar('/n';
+			return (0);
+			}
