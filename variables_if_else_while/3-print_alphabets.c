a4fddb66a57c1f21a79cb947163c91f5eade9432
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-23 16:48:53 -0400

    wrote a program that prints the alphabet in upper case and lower case followed by a new line

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point working with putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	/* loop with ASCII value of the lowercase letter (a-z)*/
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter = (a-z); /*this will print each letter in lowercase, one by one*/
+
+				/* loop with ASCII value of the uppercase letter (A-Z)*/
+				for (letter = 'A'; latter <= 'Z'; letter++)
+				putchar(letter = (A-Z); /*this will print each letter in uppercase, one by one*/
+
+
+					putchar('\n'); /*this prints a new line after the alphabet code*/
+					return (0);
