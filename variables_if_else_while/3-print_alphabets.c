efcbf4acaacf68a21a67fd3cb14621e89b427c50
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-23 18:07:17 -0400

    forgot to close the brace

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,14 @@ int main(void)
 
 	/* loop with ASCII value of the lowercase letter (a-z)*/
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter = (a-z); /*this will print each letter in lowercase, one by one*/
+		putchar(letter); /*this will print each letter in lowercase, one by one*/
+	return (0);
 
-				/* loop with ASCII value of the uppercase letter (A-Z)*/
-				for (letter = 'A'; latter <= 'Z'; letter++)
-				putchar(letter = (A-Z); /*this will print each letter in uppercase, one by one*/
+	/* loop with ASCII value of the uppercase letter (A-Z)*/
+	for (letter = 'A'; latter <= 'Z'; letter++)
+		putchar(letter); /*this will print each letter in uppercase, one by one*/
 
 
-					putchar('\n'); /*this prints a new line after the alphabet code*/
-					return (0);
-					}
+	putchar('\n'); /*this prints a new line after the alphabet code*/
+	return (0);
+}
