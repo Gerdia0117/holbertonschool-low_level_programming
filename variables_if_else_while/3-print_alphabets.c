11dd45205603748e4da47279d2e933bed9aa0d63
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-23 18:15:36 -0400

    removed some comments that were not needed

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-
 /**
  * main - Entry point working with putchar
  * Return: Always 0 (success)
  */
 int main(void)
 {
+	/* variable character/letters*/
 	char letter;
 
-	/* loop with ASCII value of the lowercase letter (a-z)*/
+	/* loop with ASCII value of the lowercase letters (a-z)*/
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter); /*this will print each letter in lowercase, one by one*/
-	return (0);
-
-	/* loop with ASCII value of the uppercase letter (A-Z)*/
+		putchar(letter); /*this is going  print each letter, one by one*/
+	/* loop with ASCII value of the uppercase letters (A-Z)*/
 	for (letter = 'A'; latter <= 'Z'; letter++)
-		putchar(letter); /*this will print each letter in uppercase, one by one*/
-
+		putchar(letter);
 
 	putchar('\n'); /*this prints a new line after the alphabet code*/
 	return (0);
