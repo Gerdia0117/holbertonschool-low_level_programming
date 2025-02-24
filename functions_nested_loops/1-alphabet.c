84d3b810c903cf71eb3eccde78b3e3dbb3a29f19
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-23 20:41:28 -0400

    copied the example

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include 'main.h'
+
+/**
+ *main - Entry point
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int abc;
+	/*this will print the full alphabet with a new line at the end*/
+	for (abc = 'a'; abd <= 'z'; abc++)
+		_putchar(abc);
+	_putchar('\n');
+
