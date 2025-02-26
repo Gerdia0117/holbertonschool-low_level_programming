39e13d85166f2315df93fe18c867215c2dedd8bc
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-26 11:10:25 -0400

    wrote a function that multiplies a and b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Mul - will multiply two integers, a and b
+ *@a: a times b
+ *@b: b time a
+ *
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
