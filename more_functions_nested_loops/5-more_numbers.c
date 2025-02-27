c1aa87a62f6a653b0d3a68992c77e2ec7672b3eb
Gerdia0117 <Gerald.froz@gmail.com>
2025-02-27 15:55:47 -0400

    added necesary functions to the script

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,19 @@ void more_numbers(void)
 	int i = 0;
 	int a;
 
-	while (i <= 14)
+	while (i <= 10)
 	{
 		for (a = 0; a <= 14; a++)
-			_putchar('a');
 		{
-			i++;
-			_putchar('\n');
+			if (a >= 10)
+			{
+				_putchar(a / 10 + '0');
+				_putchar(a % 10 + '0');
+			}
+			_putchar(a + '0');
 		}
+		i++;
+		_putchar('\n');
 	}
 
 }
