5a4b236515aff048195e03b5a634c0b72c10d21b
Gerdia0117 <Gerald.froz@gmail.com>
2025-03-01 19:58:03 -0400

    wrong file name

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print square of # character.
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int i a;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (a = 0; a < size; a++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
