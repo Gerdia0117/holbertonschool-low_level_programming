8e8b6b31473bed2abd0f437478b1c2dd1c63ec87
Gerdia0117 <Gerald.froz@gmail.com>
2025-03-02 19:38:43 -0400

    forgot to push

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ * This function loops through each character in the string and prints
+ * it using _putchar until the null-terminator is reached. A newline is
+ * printed after the string.
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
