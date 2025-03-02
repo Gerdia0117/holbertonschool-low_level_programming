7bb033941e3fe33bebca48f2cc3c3c387795dc14
Gerdia0117 <Gerald.froz@gmail.com>
2025-03-01 22:18:24 -0400

    forgot to puch work

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle of # characters
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, a;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (a = 1; a <= size - 1; a++)
+			{
+				_putchar(' ');
+			}
+
+			for (a = 1; a <= i; a++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
