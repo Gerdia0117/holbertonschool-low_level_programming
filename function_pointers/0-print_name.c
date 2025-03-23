e1a07763a6780be3345a89ed4809e3f8f24118af
Gerdia0117 <Gerald.froz@gmail.com>
2025-03-23 15:04:32 -0400

    trying to fix a problem with 0 print file

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,8 @@
-#include <stddef.h>
 #include "function_pointers.h"
-
+#include <stddef.h>
 /**
- * print_name - calls function to print a name
- *@name: name to be  printed
+ *print_name - call function to print a name
+ *@name: name to be printed
  *@f: a pointer to a function that prints a name
  *Return: Nothing
  */
